61c0a2e91be7ef55c7455fe6fc3db55ca53e7346
Rafael Infante <rafael@telcluc.net>
2024-01-06 13:14:08 +0000

    Adding routing and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ContactComponent} from "./contact/contact.component";
+import {FeaturesComponent} from "./features/features.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, HomeComponent, ContactComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FeaturesComponent} from "./features/features.component";
+import {ContactComponent} from "./contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"features", component: FeaturesComponent},
+  {path:"contact", component: ContactComponent},
+  {path:"**", redirectTo: ""}
+];
