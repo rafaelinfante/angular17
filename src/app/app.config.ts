0f616e4ef4b97f98b6372f5da498fa80b55b71c8
Rafael Infante <rafael@telclic.net>
2024-01-17 06:18:08 +0000

    i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()],
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
+    provideHttpClient(withFetch()),
+  ],
 };
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { SignInProviders } from '../../_shared/interfaces/auth-interfaces';
 import { SIGNIN_PROVIDERS } from '../../_shared/configs/signin-buttons.config';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   public signInProviders: SignInProviders[] = SIGNIN_PROVIDERS;
+
+  constructor(private translate: TranslateService) {
+    this.translate.use('pt');
+  }
 }
